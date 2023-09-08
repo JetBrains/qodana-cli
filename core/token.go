ef928b5e0204cf90907ae0ec927441bd45f85dfc
tiulpin <viktor@tiulp.in>
2023-09-08 16:05:55 +0200

    :bug: Fail when token is provided and invalid

--- core/token.go
+++ core/token.go
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2021-2023 JetBrains s.r.o.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package core
 
 import (
@@ -76,7 +92,10 @@ func (o *QodanaOptions) ValidateToken(refresh bool) string {
 	token := o.loadToken(refresh)
 	client := cloud.NewQodanaClient()
 	if projectName := client.ValidateToken(token); projectName == "" {
-		WarningMessage(cloud.InvalidTokenMessage)
+		if token != "" {
+			ErrorMessage(cloud.InvalidTokenMessage)
+			os.Exit(1)
+		}
 	} else {
 		SuccessMessage("Linked project name: %s", projectName)
 		o.setenv(QodanaToken, token)
