a62b465ebf16aa7aac39e073985aeaf4d8bbe7a6
tiulpin <viktor@tiulp.in>
2024-04-16 13:24:41 +0200

    :bug: Fix `--version` showing and update release hooks

--- core/system.go
+++ core/system.go
@@ -47,7 +47,7 @@ var (
 
 // CheckForUpdates check GitHub https://github.com/JetBrains/qodana-cli/ for the latest version of CLI release.
 func CheckForUpdates(currentVersion string) {
-	if currentVersion == "dev" || platform.IsContainer() || cienvironment.DetectCIEnvironment() != nil || DisableCheckUpdates {
+	if currentVersion == "dev" || strings.HasSuffix(currentVersion, "nightly") || platform.IsContainer() || cienvironment.DetectCIEnvironment() != nil || DisableCheckUpdates {
 		return
 	}
 	latestVersion := getLatestVersion()
