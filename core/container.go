3caafd67545a62b1fdf8cca0e4a016372a14c114
Mikhail Shagvaliev <mikhail.shagvaliev@jetbrains.com>
2024-08-06 11:48:45 +0200

    :zip: my shiny feature

--- core/container.go
+++ core/container.go
@@ -96,6 +96,12 @@ func runQodanaContainer(ctx context.Context, options *QodanaOptions) int {
 	return int(exitCode)
 }
 
+func unusedFunction() {
+	for {
+
+	}
+}
+
 func fixDarwinCaches(options *QodanaOptions) {
 	if //goland:noinspection GoBoolExpressions
 	runtime.GOOS == "darwin" {
