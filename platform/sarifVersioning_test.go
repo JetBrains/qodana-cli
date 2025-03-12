d08f48b41f9cebee7e7505a8063e5f41060d4348
Anna Zhukova <anna.zhukova@jetbrains.com>
2025-03-12 18:10:59 +0100

    :white_check_mark: QD-10662: Add tests for getting branch from environment

--- platform/sarifVersioning_test.go
+++ platform/sarifVersioning_test.go
@@ -17,13 +17,14 @@
 package platform
 
 import (
-	"github.com/JetBrains/qodana-cli/v2025/platform/utils"
 	"os"
 	"testing"
+
+	"github.com/JetBrains/qodana-cli/v2025/platform/utils"
 )
 
 func TestGetBranchName(t *testing.T) {
-	dir, err := os.MkdirTemp("", "test-repository")
+	dir, err := os.MkdirTemp("", "repo-TestGetBranchName")
 	if err != nil {
 		t.Fatal(err)
 	}
@@ -34,14 +35,7 @@ func TestGetBranchName(t *testing.T) {
 		}
 	}(dir)
 
-	_, _, ret, err := utils.RunCmdRedirectOutput(dir, "git", "init", "--initial-branch=my-branch")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if ret != 0 {
-		t.Fatalf("git init failed with exit code %d", ret)
-	}
-
+	runCommand(t, dir, "git", "init", "--initial-branch=my-branch")
 	branch, err := getBranchName(dir)
 	if err != nil {
 		t.Fatal(err)
@@ -50,14 +44,7 @@ func TestGetBranchName(t *testing.T) {
 		t.Fatalf("Incorrect branch name: '%s' (expected 'my-branch')", branch)
 	}
 
-	_, _, ret, err = utils.RunCmdRedirectOutput(dir, "git", "commit", "--allow-empty", "-m", "commit")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if ret != 0 {
-		t.Fatalf("git commit failed with exit code %d", ret)
-	}
-
+	runCommand(t, dir, "git", "commit", "--allow-empty", "-m", "commit")
 	branch, err = getBranchName(dir)
 	if err != nil {
 		t.Fatal(err)
@@ -66,19 +53,76 @@ func TestGetBranchName(t *testing.T) {
 		t.Fatalf("Incorrect branch name: '%s' (expected 'my-branch')", branch)
 	}
 
-	_, _, ret, err = utils.RunCmdRedirectOutput(dir, "git", "switch", "--detach")
-	if err != nil {
-		t.Fatal(err)
-	}
-	if ret != 0 {
-		t.Fatalf("git switch failed with exit code %d", ret)
-	}
-
+	runCommand(t, dir, "git", "switch", "--detach")
 	branch, err = getBranchName(dir)
 	if err != nil {
 		t.Fatal(err)
 	}
 	if branch != "" {
-		t.Fatalf("Incorrect branch name: '%s' (expected '<empty>')", branch)
+		t.Fatalf("Incorrect branch name: '%s' (expected <empty>)", branch)
 	}
+}
+
+func TestGetVersionDetailsBranchFromEnvironment(t *testing.T) {
+	dir, err := os.MkdirTemp("", "repo-TestGetVersionDetailsBranch")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func(path string) {
+		err := os.RemoveAll(path)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}(dir)
+
+	assertBranchName := func(expected string) {
+		version_info, err := GetVersionDetails(dir)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if version_info.Branch != expected {
+			t.Fatalf("Incorrect branch name: '%s' (expected '%s')", version_info.Branch, expected)
+		}
+	}
+
+	runCommand(t, dir, "git", "init", "--initial-branch=my-branch")
+	runCommand(t, dir, "git", "commit", "--allow-empty", "-m", "commit")
+	runCommand(t, dir, "git", "switch", "--detach")
+
+	os.Setenv("QODANA_BRANCH", "QODANA_BRANCH")
+	os.Setenv("CI", "true")
+	os.Setenv("CI_COMMIT_BRANCH", "CI_COMMIT_BRANCH")
+	assertBranchName("QODANA_BRANCH")
+
+	os.Setenv("QODANA_BRANCH", "QODANA_BRANCH")
+	os.Unsetenv("CI")
+	os.Unsetenv("CI_COMMIT_BRANCH")
+	assertBranchName("QODANA_BRANCH")
+
+	os.Unsetenv("QODANA_BRANCH")
+	os.Setenv("CI", "true")
+	os.Setenv("CI_COMMIT_BRANCH", "CI_COMMIT_BRANCH")
+	assertBranchName("CI_COMMIT_BRANCH")
+
+	os.Unsetenv("QODANA_BRANCH")
+	os.Setenv("CI", "false")
+	os.Setenv("CI_COMMIT_BRANCH", "CI_COMMIT_BRANCH")
+	assertBranchName("")
+
+	os.Unsetenv("QODANA_BRANCH")
+	os.Unsetenv("CI")
+	os.Setenv("CI_COMMIT_BRANCH", "CI_COMMIT_BRANCH")
+	assertBranchName("")
+}
+
+func runCommand(t *testing.T, cwd string, args ...string) (string, string) {
+	stdout, stderr, ret, err := utils.RunCmdRedirectOutput(cwd, args...)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if ret != 0 {
+		t.Fatalf("%q failed with exit code %d", args, ret)
+	}
+
+	return stdout, stderr
 }
