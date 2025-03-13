c5197be015bff4d8b05390d5d9e9883d83fbee78
Andrei Iurko <andrei.iurko@jetbrains.com>
2025-03-13 23:48:10 +0300

    :bug: QD-11266 Pass corrected ResultsDir for permissions correction

--- platform/run.go
+++ platform/run.go
@@ -42,8 +42,6 @@ func RunThirdPartyLinterAnalysis(
 	linterInfo thirdpartyscan.LinterInfo,
 ) (int, error) {
 	var err error
-	resultDir := cliOptions.ResultsDir
-	defer changeResultDirPermissionsInContainer(resultDir)
 
 	commonCtx := commoncontext.Compute(
 		cliOptions.Linter,
@@ -61,7 +59,8 @@ func RunThirdPartyLinterAnalysis(
 		msg.ErrorMessage(err.Error())
 		return 1, err
 	}
-	resultDir = commonCtx.ResultsDir
+	resultDir := commonCtx.ResultsDir
+	defer changeResultDirPermissionsInContainer(resultDir)
 
 	thirdPartyCloudData := checkLinterLicense(commonCtx)
 	isCommunity := thirdPartyCloudData.LicensePlan == cloud.CommunityLicensePlan
