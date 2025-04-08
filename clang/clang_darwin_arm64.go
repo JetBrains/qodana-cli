3793f6fd18682e11264d3a9b8f071bb3204f8672
Anna Zhukova <anna.zhukova@jetbrains.com>
2025-04-08 18:52:07 +0200

    :sparkles: QD-11458: Embed clang-tidy hashes in addition to binaries themselves

--- clang/clang_darwin_arm64.go
+++ clang/clang_darwin_arm64.go
@@ -4,6 +4,12 @@ package main
 
 import _ "embed"
 
+//go:generate go run scripts/hash-contents.go clang-mac-aarch64.tar.gz
+
 //go:embed clang-mac-aarch64.tar.gz
 var Clang []byte
+
+//go:embed clang-mac-aarch64.sha256.bin
+var Hash []byte
+
 var Ext = ".tar.gz"
