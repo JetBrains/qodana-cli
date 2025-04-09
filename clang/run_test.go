d59c915d47f98697d53f7f1fc6b0b9a8ae05fbd6
Anna Zhukova <anna.zhukova@jetbrains.com>
2025-04-09 18:57:25 +0200

    :white_check_mark: QD-11458: Add a test for correct hash

---
+++ clang/run_test.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"os"
+	"testing"
+
+	"github.com/JetBrains/qodana-cli/v2025/platform/thirdpartyscan"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestMountTools(t *testing.T) {
+	linter := ClangLinter{}
+	tempdir, err := os.MkdirTemp("", "TestMountTools")
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer func() {
+		err = os.RemoveAll(tempdir)
+		if err != nil {
+			t.Fatal(err)
+		}
+	}()
+
+	mountInfo, err := linter.MountTools(tempdir)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	path := mountInfo[thirdpartyscan.Clang]
+	expectedHash := Hash
+	actualHash, err := getSha256(path)
+	if err != nil {
+		t.Fatal(err)
+	}
+	assert.Equal(t, expectedHash, actualHash)
+}
