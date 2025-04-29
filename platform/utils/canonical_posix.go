b9413d760d896d2af425213261856c5c63657360
Anna Zhukova <anna.zhukova@jetbrains.com>
2025-04-29 17:14:22 +0200

    :bug: QD-11565: Fix errno checked before return

--- platform/utils/canonical_posix.go
+++ platform/utils/canonical_posix.go
@@ -12,7 +12,7 @@ func Canonical(path string) (result string, err error) {
 	pathCString := C.CString(path)
 	defer C.free(unsafe.Pointer(pathCString))
 	resultCString, errno := C.realpath(pathCString, nil)
-	if errno != nil {
+	if resultCString == nil {
 		return "", errno
 	}
 	defer C.free(unsafe.Pointer(resultCString))
