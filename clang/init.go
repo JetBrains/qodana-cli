48a7217a9a091b1ba78655426986ccd3003603d2
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2025-06-28 01:38:06 +0200

    :brics: QD-10741 Fix qodana-clang

--- clang/init.go
+++ clang/init.go
@@ -3,20 +3,26 @@ package main
 import (
 	"github.com/JetBrains/qodana-cli/v2025/cmd"
 	"github.com/JetBrains/qodana-cli/v2025/platform"
+	"github.com/JetBrains/qodana-cli/v2025/platform/product"
 	"github.com/JetBrains/qodana-cli/v2025/platform/thirdpartyscan"
 	"github.com/spf13/cobra"
 )
 
-func Execute(productCode string, linterName string, linterVersion string, buildDateStr string, isEap bool) {
+func Execute(
+	linterVersion string,
+	buildDateStr string,
+	isEap bool,
+) {
 	platform.CheckEAP(buildDateStr, isEap)
 
 	linter := ClangLinter{}
 
 	linterInfo := thirdpartyscan.LinterInfo{
-		ProductCode:   productCode,
-		LinterName:    linterName,
-		LinterVersion: linterVersion,
-		IsEap:         isEap,
+		ProductCode:           product.ClangLinter.ProductCode,
+		LinterName:            product.ClangLinter.Name,
+		LinterPresentableName: product.ClangLinter.PresentableName,
+		LinterVersion:         linterVersion,
+		IsEap:                 isEap,
 	}
 
 	commands := make([]*cobra.Command, 1)
