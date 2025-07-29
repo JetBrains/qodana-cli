adc18cf7ca9796f1a85d5d3996139ede746c3dcd
Anna Zhukova <28830446+bindreams@users.noreply.github.com>
2025-07-29 19:10:33 +0200

    :bug: QD-12266: Fix qodana-cdnet build process (#629)
    * :bug: QD-12266: Fix qodana-cdnet building even when required zip contents were not found
    
    * :bug: QD-12266: Minor formatting fix
    
    * :bug: QD-12266: Support variable names of dll inside clt.zip
    
    * :bug: Fix mock clt.zip behavior
    
    (cherry picked from commit d8ce4aed9031efc4f5d088fb145d18d1629e7d39)

--- cdnet/scripts/process-cltzip.go
+++ cdnet/scripts/process-cltzip.go
@@ -7,28 +7,34 @@ import (
 	"io"
 	"log"
 	"os"
+	"regexp"
 
 	"github.com/JetBrains/qodana-cli/v2025/platform/utils"
 )
 
-func main() {
-	if len(os.Args) <= 1 {
-		log.Fatalf("Expected path of the file to hash as argv[1] (relative from clt.zip root)")
-	}
-	targetPath := os.Args[1]
+const DLL_PATH_PATTERN = `^tools/[^/]+/any/JetBrains\.CommandLine\.Products\.dll$`
 
-	// Compute hash for the clang-tidy binary
-	var hash [32]byte
+var dllPathRegex = regexp.MustCompile(DLL_PATH_PATTERN)
+
+func main() {
+	// Find and hash commandline tools DLL inside the archive
+	var dllPath = ""
+	var dllHash [32]byte
 	callback := func(path string, info os.FileInfo, stream io.Reader) {
 		if info.IsDir() {
 			return
 		}
-		if path != targetPath {
+		if !dllPathRegex.MatchString(path) {
 			return
 		}
 
+		if dllPath != "" {
+			log.Fatalf("Found multiple matches for `%s` inside clt.zip: '%s', '%s'.", DLL_PATH_PATTERN, dllPath, path)
+		}
+		dllPath = path
+
 		err := (error)(nil)
-		hash, err = utils.GetSha256(stream)
+		dllHash, err = utils.GetSha256(stream)
 		if err != nil {
 			log.Fatalf("sha256 error: %s", err)
 		}
@@ -49,14 +55,22 @@ func main() {
 		if err != nil {
 			log.Fatal(err)
 		}
+		if dllPath == "" {
+			log.Fatalf("Could not find a file matching `%s` DLL inside clt.zip.", DLL_PATH_PATTERN)
+		}
 	}
 
-	err = os.WriteFile("clt.sha256.bin", hash[:], 0666)
+	err = os.WriteFile("clt.sha256.bin", dllHash[:], 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	_, err = fmt.Fprintf(os.Stderr, "sha256 of the contents of clt.zip: %s\n", hex.EncodeToString(hash[:]))
+	_, err = fmt.Fprintf(os.Stderr, "sha256 of clt.zip/%s: %s\n", dllPath, hex.EncodeToString(dllHash[:]))
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = os.WriteFile("clt.path.txt", []byte(dllPath), 0666)
 	if err != nil {
 		log.Fatal(err)
 	}
