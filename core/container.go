3e945cd05113ab2337dd3c0be778a9deaa9136ec
Anna Zhukova <anna.zhukova@jetbrains.com>
2025-09-02 17:32:45 +0200

    Do not set default user in privileged images

--- core/container.go
+++ core/container.go
@@ -21,14 +21,16 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
-	"github.com/JetBrains/qodana-cli/v2025/cloud"
-	"github.com/docker/docker/api/types/network"
 	"io"
 	"os"
 	"path/filepath"
+	"regexp"
 	"runtime"
 	"strconv"
 	"strings"
+
+	"github.com/JetBrains/qodana-cli/v2025/cloud"
+	"github.com/docker/docker/api/types/network"
 
 	"github.com/JetBrains/qodana-cli/v2025/core/corescan"
 	"github.com/JetBrains/qodana-cli/v2025/platform/msg"
@@ -36,6 +38,7 @@ import (
 	"github.com/JetBrains/qodana-cli/v2025/platform/qdcontainer"
 	"github.com/JetBrains/qodana-cli/v2025/platform/qdenv"
 	"github.com/JetBrains/qodana-cli/v2025/platform/strutil"
+	"github.com/JetBrains/qodana-cli/v2025/platform/utils"
 	"github.com/JetBrains/qodana-cli/v2025/platform/version"
 	"github.com/docker/docker/api/types/backend"
 	"github.com/docker/docker/api/types/registry"
@@ -423,11 +426,25 @@ func getDockerOptions(c corescan.Context, image string) *backend.ContainerCreate
 			AttachStdout: true,
 			AttachStderr: true,
 			Env:          dockerEnv,
-			User:         c.User(),
+			User:         selectUser(image, c.User()),
 			ExposedPorts: exposedPorts,
 		},
 		HostConfig: hostConfig,
 	}
+}
+
+var rePrivilegedImage = regexp.MustCompile(`^(jetbrains|registry.jetbrains.team)/.+-privileged.*$`)
+
+func selectUser(image string, userFromContext string) string {
+	if userFromContext == "auto" {
+		if !rePrivilegedImage.MatchString(image) {
+			return utils.GetDefaultUser()
+		}
+
+		return "" // Do not specify -u on the command line.
+	}
+
+	return userFromContext // Do not modify explicit user input
 }
 
 func generateDebugDockerRunCommand(cfg *backend.ContainerCreateConfig) string {
