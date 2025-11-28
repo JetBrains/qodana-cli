6f7a8862919800ab6c7226abec76cef20fe124b3
tiulpin <viktor@tiulp.in>
2025-11-28 17:26:58 +0100

    :bricks: QD-12501 Update TeamCity settings

--- .teamcity/cli/Cli.kt
+++ .teamcity/cli/Cli.kt
@@ -1,15 +1,5 @@
 package cli
 
-import jetbrains.buildServer.configs.kotlin.Project
-
-object CLI : Project({
-    id("Cli")
-    name = "CLI"
-    description = "Various products built from https://github.com/jetbrains/qodana-cli"
-    buildType(GoReleaser("cli", "", arguments = listOf()))
-    buildType(GoReleaser("cli", "main", arguments = listOf("--nightly", "--skip=chocolatey,nfpm,homebrew,scoop,snapcraft")))
-})
-
 const val clangId = "ijplatform_master_CIDR_ExternalTools_ClangdAll"
 
 fun getDotNetId(branch: String): String {
