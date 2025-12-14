f2ec2d4535b2e337338d7663e714cb37014219fc
alexey.afanasiev <alexey.afanasiev@jetbrains.com>
2025-12-14 21:55:50 +0100

    :bug: QD-13118 Rename --source-directory to --only-directory and pass --only-directory to intellij engine

--- internal/platform/cmd/scan_options.go
+++ internal/platform/cmd/scan_options.go
@@ -38,7 +38,7 @@ type CliOptions struct {
 	Image                     string
 	WithinDocker              string
 	Ide                       string
-	SourceDirectory           string
+	OnlyDirectory             string
 	DisableSanity             bool
 	ProfileName               string
 	ProfilePath               string
@@ -232,9 +232,15 @@ func ComputeFlags(cmd *cobra.Command, options *CliOptions) error {
 		false,
 		"Skip running the inspections configured by the sanity profile",
 	)
-	flags.StringVarP(
-		&options.SourceDirectory,
+	flags.StringVar(
+		&options.OnlyDirectory,
 		"source-directory",
+		"",
+		"Deprecated. Use --only-directory instead.",
+	)
+	flags.StringVarP(
+		&options.OnlyDirectory,
+		"only-directory",
 		"d",
 		"",
 		"Directory inside the project-dir directory must be inspected. If not specified, the whole project is inspected",
@@ -397,6 +403,7 @@ func ComputeFlags(cmd *cobra.Command, options *CliOptions) error {
 	cmd.MarkFlagsMutuallyExclusive("commit", "script", "diff-start")
 	cmd.MarkFlagsMutuallyExclusive("profile-name", "profile-path")
 	cmd.MarkFlagsMutuallyExclusive("apply-fixes", "cleanup")
+	cmd.MarkFlagsMutuallyExclusive("source-directory", "only-directory")
 
 	if err = cmd.Flags().MarkDeprecated("fixes-strategy", "use --apply-fixes / --cleanup instead"); err != nil {
 		return err
@@ -404,6 +411,12 @@ func ComputeFlags(cmd *cobra.Command, options *CliOptions) error {
 	if err = cmd.Flags().MarkDeprecated(
 		"ide",
 		"use --linter with corresponding linter type and --within-docker=false instead",
+	); err != nil {
+		return err
+	}
+	if err = cmd.Flags().MarkDeprecated(
+		"source-directory",
+		"--source-directory is deprecated, use --only-directory instead",
 	); err != nil {
 		return err
 	}
