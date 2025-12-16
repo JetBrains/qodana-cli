bb79f7fb5042a0b6c031c1ed05546cd504c91605
Anna Zhukova <anna.zhukova@jetbrains.com>
2025-12-16 19:13:50 +0100

    Embed clang test data in the test executable
    I hate go

---
+++ internal/platform/fsutil/fsutil_test.go
@@ -0,0 +1,123 @@
+package fsutil
+
+import (
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+// tempFs creates a filesystem of empty files and dirs according to the relative paths that it should contain.
+func tempFs(t *testing.T, paths []string) fs.FS {
+	root := t.TempDir()
+
+	for _, path := range paths {
+		path = root + "/" + path
+		parents, filename := filepath.Split(path)
+
+		if parents != "" {
+			os.MkdirAll(parents, os.ModePerm)
+		}
+
+		if filename != "" {
+			Touch(path)
+		}
+	}
+
+	return os.DirFS(root)
+}
+
+func TestSubDir(t *testing.T) {
+	t.Run("basic", func(t *testing.T) {
+		root := tempFs(t, []string{
+			"a/b/c",
+		})
+
+		a, err := SubDir(root, "a")
+		require.NoError(t, err)
+
+		_, err = SubDir(a, "b")
+		require.NoError(t, err)
+
+		_, err = SubDir(root, "a/b")
+		require.NoError(t, err)
+	})
+
+	t.Run("empty directory", func(t *testing.T) {
+		root := tempFs(t, []string{
+			"a/b/",
+		})
+
+		a, err := SubDir(root, "a")
+		require.NoError(t, err)
+		require.NotNil(t, a)
+
+		b, err := SubDir(a, "b")
+		require.NoError(t, err)
+		require.NotNil(t, b)
+
+		b2, err := SubDir(root, "a/b")
+		require.NoError(t, err)
+		require.NotNil(t, b2)
+	})
+
+	t.Run("path does not exist", func(t *testing.T) {
+		root := tempFs(t, []string{
+			"a/b/c",
+		})
+
+		val, err := SubDir(root, "b")
+		require.ErrorIs(t, err, fs.ErrNotExist)
+		require.Nil(t, val)
+
+		val, err = SubDir(root, "a/x")
+		require.ErrorIs(t, err, fs.ErrNotExist)
+		require.Nil(t, val)
+
+		val, err = SubDir(root, "a/b/x")
+		require.ErrorIs(t, err, fs.ErrNotExist)
+		require.Nil(t, val)
+	})
+
+	t.Run("path is not a directory", func(t *testing.T) {
+		root := tempFs(t, []string{
+			"a/b/c",
+			"file",
+		})
+
+		b, err := SubDir(root, "a/b")
+		require.NoError(t, err)
+		c, err := SubDir(b, "c")
+		require.Error(t, err)
+		require.Nil(t, c)
+
+		c2, err := SubDir(root, "a/b/c")
+		require.Error(t, err)
+		require.Nil(t, c2)
+
+		file, err := SubDir(root, "file")
+		require.Error(t, err)
+		require.Nil(t, file)
+	})
+}
+
+func TestTouch(t *testing.T) {
+	t.Run("happy path", func(t *testing.T) {
+		dir := t.TempDir()
+		path := dir + "/file"
+
+		require.NoError(t, Touch(path))
+
+		stat1, err := os.Stat(path)
+		require.NoError(t, err)
+
+		require.NoError(t, Touch(path))
+
+		stat2, err := os.Stat(path)
+		require.NoError(t, err)
+
+		require.Greater(t, stat2.ModTime(), stat1.ModTime())
+	})
+}
