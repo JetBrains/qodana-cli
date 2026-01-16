8bd248944bad9d9cbde77a0ea2aa305aace38a82
Anna Zhukova <anna.zhukova@jetbrains.com>
2026-01-16 17:40:05 +0100

    Use public repositories of baseline-cli and qodana-fuser

--- .teamcity/cli/Artifacts.kt
+++ .teamcity/cli/Artifacts.kt
@@ -33,34 +33,34 @@ fun BuildSteps.denestFileArtifacts(searchStartDir: String = "%teamcity.build.che
         scriptContent = """
             #!/usr/bin/env bash
             set -euo pipefail
-            
+
             denest() {
                 directory="$1"
-                
+
                 list_artifacts() {
                     find "${'$'}directory" -mindepth 1 -maxdepth 1 "$@"
                 }
-                
+
                 n_files=$(list_artifacts -printf '.' | wc -c)
                 if [ ${'$'}{n_files} -eq 0 ]; then
                     echo "No files found in '$(pwd)/${'$'}directory'. This is likely an error in BuildSteps.denestFileArtifacts." >&2
                     exit 2
                 fi
-                
+
                 if [ ${'$'}{n_files} -ne 1 ]; then
                     echo "Found ${'$'}{n_files} files matching '$(pwd)/${'$'}directory':" >&2
                     list_artifacts >$2
                     echo "IJBuildType.loadSingleFileArtifact pattern should match a single file." >&2
                     exit 1
                 fi
-                
+
                 targetPath="$(dirname "${'$'}directory")/$(basename "${'$'}directory" "$denestSuffix")"
                 list_artifacts -exec mv -vT {} "${'$'}targetPath" \;
                 rm -r "${'$'}directory"
             }
-            
+
             # see also: https://stackoverflow.com/q/9612090
-            find . -name "*$denestSuffix" -print0 | while IFS= read -r -d '' directory; do 
+            find . -name "*$denestSuffix" -print0 | while IFS= read -r -d '' directory; do
                 denest "${'$'}directory"
             done
         """.trimIndent()
@@ -70,24 +70,6 @@ fun BuildSteps.denestFileArtifacts(searchStartDir: String = "%teamcity.build.che
 internal const val THIRD_PARTY_LICENSES_RULE = "**/*third-party-libraries.json=>licenses/"
 
 fun Dependencies.getQodanaToolingArtifacts(tool: String = "cli") {
-    artifacts(AbsoluteId("StaticAnalysis_Base_Build_baseline_cli")) {
-        buildRule = lastSuccessful()
-        artifactRules = """
-            baseline-cli-*.jar => internal/tooling/baseline-cli.jar$DENEST_SUFFIX/
-        """.trimIndent()
-        if (tool != "cli") {
-            artifactRules += "\n\n$THIRD_PARTY_LICENSES_RULE"
-        }
-    }
-    artifacts(AbsoluteId("StaticAnalysis_Base_Build_fuser")) {
-        buildRule = lastSuccessful()
-        artifactRules = """
-            qodana-fuser-*.jar => internal/tooling/qodana-fuser.jar$DENEST_SUFFIX/
-        """.trimIndent()
-        if (tool != "cli") {
-            artifactRules += "\n\n$THIRD_PARTY_LICENSES_RULE"
-        }
-    }
     artifacts(AbsoluteId("StaticAnalysis_Build_UiAndConverter")) {
         buildRule = tag("readyForTest", "+:*")
         artifactRules = """
