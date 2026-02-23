17e17ab86601e3fded69e378632f1f19a11b27fa
Jakub Andrzejewski <jakub.andrzejewski@jetbrains.com>
2026-02-23 14:58:14 +0100

    :sparkles: docker diagnostics

--- test_linter/3rd_party_linter_jbr_integration_test.go
+++ test_linter/3rd_party_linter_jbr_integration_test.go
@@ -283,8 +283,9 @@ func runQodanaScan(t *testing.T, containerID string) string {
 
 	cmd := exec.CommandContext(ctx, args[0], args[1:]...)
 	output, err := cmd.CombinedOutput()
+	t.Logf("Qodana scan output:\n%s", string(output))
 	if err != nil {
-		t.Logf("Qodana scan output:\n%s", string(output))
+		t.Logf("Qodana scan error: %v", err)
 	}
 	require.NoError(t, err, "Qodana scan failed")
 	return string(output)
